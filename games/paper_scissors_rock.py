65fe23db3f0fc1e976943fe930b7805984bcdd27
MannyDiera <mdiera.dvk@gmail.com>
2021-02-13 11:24:20 -0500

    Added changes to paper scissors rock game

--- games/paper_scissors_rock.py
+++ games/paper_scissors_rock.py
@@ -1,69 +1,63 @@
 # First python game
 
-import random, sys
+import random
+from sys import exit
 
 # Game setup
-# options
-# gameOptions = ['rock', 'paper', 'scissors', 'quit']
-
 wins = 0
 looses = 0
 ties = 0
 
-print('Lets play paper scissors rock! \nEnter your choice as: rock, paper, scissors or quit :')
-playMover = input()
-
-# Print out what user selected vs
-if playMover == 'quit':
-    print('Ok, game cancelled')
-    sys.exit()
-else:
-    print(f'{playMover.title()} versus...')
+computerOptions = ['quit', 'rock', 'paper', 'scissors']
 
 
-compuMover = ['rock', 'paper', 'scissors', 'quit']
+def get_player_move():
+    print('Lets play paper scissors rock! \nEnter your choice as: rock, paper, scissors or quit :')
+    player_input = input()
 
-randomNumber = random.randint(1, 4)
-
-if randomNumber == 1:
-    compuMove = 'rock'
-    print('ROCK')
-
-elif randomNumber == 2:
-    compuMover = 'paper'
-    print('PAPER')
-
-elif randomNumber == 3:
-    compuMover = 'scissors'
-    print('SCISSORS')
-
+    # Print out what user selected vs
+    if player_input not in computerOptions:
+        print('Oops, that option is not valid. Try again')
+        player_input = input()
+    if player_input == 'quit':
+        print('Ok, game cancelled')
+        exit()
+    else:
+        print(f'{player_input.title()} versus...')
+        return player_input
 
 
+def log_user_move():
+    computer_move = computerOptions[random.randint(1, 3)]
+    print(f'{computer_move.upper()}')
+    return computer_move
 
-if playMover == compuMover:
-    print('Is a tie')
-    ties = ties + 1
 
-elif playMover == 'rock' and compuMover == 'scissors':
-    print('you win')
-    wins = wins + 1
+def check_result(player, computer):
+    print(f'playerMove is: {player} and computerMove is: {computer}')
+    if playerMove == computerMove:
+        print('Is a tie')
 
-elif playMover == 'paper' and compuMover == 'rock':
-    print('you win')
-    wins = wins + 1
+    elif playerMove == 'rock' and computerMove == 'scissors':
+        print('you win')
 
-elif playMover == 'scissors' and compuMover == 'paper':
-    print('you win')
-    wins = wins + 1
+    elif playerMove == 'paper' and computerMove == 'rock':
+        print('you win')
 
-elif playMover == 'rock' and compuMover == 'paper':
-    print('you lose!')
-    losses = looses + 1
+    elif playerMove == 'scissors' and computerMove == 'paper':
+        print('you win')
 
-elif playMover == 'paper' and compuMover == 'scissors:':
-    print('you lose!')
-    losses = looses + 1
+    elif playerMove == 'rock' and computerMove == 'paper':
+        print('you lose!')
 
-elif playMover == 'scissors' and compuMover == 'rock':
-    print('you lose!')
-    looses = looses + 1
+    elif playerMove == 'paper' and computerMove == 'scissors':
+        print('you lose!')
+
+    elif playerMove == 'scissors' and computerMove == 'rock':
+        print('you lose!')
+
+
+playerMove = get_player_move()
+computerMove = log_user_move()
+check_result(playerMove, computerMove)
+
